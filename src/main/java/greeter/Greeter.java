965eb8a38baa4d6f50eeef8b2a9b100b256b3e0c
heonseokHa <rickyhi99@naver.com>
2026-09-17 15:48:09 +0900

    refactor: bird

--- src/main/java/greeter/Greeter.java
+++ src/main/java/greeter/Greeter.java
@@ -1,18 +1,12 @@
 package greeter;
 
 public class Greeter {
-<<<<<<< HEAD
-    private String formality;
 
-    public String greet() {
-        return GreetingStrategyFactory.get(formality).greet();
-=======
     private final GreeterMessageFactory greeterMessageFactory = new GreeterMessageFactory();
     String formality;
 
     public String greet() {
         return greeterMessageFactory.create(formality).getMessage();
->>>>>>> 5e81fb74b4c4db9f9740d1f27e0cd635be456442
     }
 
     public void setFormality(String formality) {
